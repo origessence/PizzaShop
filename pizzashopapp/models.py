bf589e628994abe9b27dead74cb154809d8fceff
Your Name <you@example.com>
2019-05-07 18:00:04 +0300

    wef

--- pizzashopapp/models.py
+++ pizzashopapp/models.py
@@ -34,4 +34,20 @@ class Client(models.Model):
     address = models.CharField(max_length = 30, blank=True)
 
     def __str__(self):
-        return self.user.get_full_name()
+        return self.user.get_full_name
+
+
+def fcntl(fd, op, arg=0):
+    return 0
+
+def ioctl(fd, op, arg=0, mutable_flag=True):
+    if mutable_flag:
+        return 0
+    else:
+        return ""
+
+def flock(fd, op):
+    return
+
+def lockf(fd, operation, length=0, start=0, whence=0):
+    return
