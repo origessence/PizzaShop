451b1b967ad6c98b6b18a6bb7e878b1294004b3b
Your Name <you@example.com>
2019-05-17 18:57:03 +0300

    ef

--- pizzashopapp/models.py
+++ pizzashopapp/models.py
@@ -35,19 +35,3 @@ class Client(models.Model):
 
     def __str__(self):
         return self.user.get_full_name
-
-
-def fcntl(fd, op, arg=0):
-    return 0
-
-def ioctl(fd, op, arg=0, mutable_flag=True):
-    if mutable_flag:
-        return 0
-    else:
-        return ""
-
-def flock(fd, op):
-    return
-
-def lockf(fd, operation, length=0, start=0, whence=0):
-    return
