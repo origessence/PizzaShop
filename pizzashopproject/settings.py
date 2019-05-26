a111561b5e9cbf206823b397e4a9eb2fc3c6286c
Your Name <you@example.com>
2019-05-26 21:43:24 +0300

    Migrar

--- pizzashopproject/settings.py
+++ pizzashopproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$+#homf4n2=ndnopd=jv#udt)8z5c_d2k5qo=_zt$mf*5x2woi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mysterious-taiga-24299.herokuapp.com','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['134.209.47.133', 'localhost', '127.0.0.1']
 
 
 # Application definition
